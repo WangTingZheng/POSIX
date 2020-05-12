39edb75471919aa3107c937eb9fd883490e21ec8
WangTingZheng <2251985371@qq.com>
2020-05-12 18:06:48 +0800

    add symlink

--- src/ls/ls.c
+++ src/ls/ls.c
@@ -14,12 +14,13 @@ struct mode
 {
     int showPoint;
     int showIgnore;
+    int isSymlink;
 };
 
 /*
  * 是否显示当前目录本身. 和上级目录 --p(point)
  * 是否显示隐藏文件 --i(ignore)
- * 是否显示符号链接文件本身的属性还是符号链接指向文件的属性
+ * 是否显示符号链接文件本身的属性还是符号链接指向文件的属性 --s(symlink)
  */
 void myls_l(char *pathname, struct mode);
 
@@ -42,6 +43,9 @@ int main(int argc, char *argv[])
 	        } else if(strcmp(argv[i], "--i") == 0)
 	        {
 	           print_mode.showIgnore = 1;
+	        }else if(strcmp(argv[i], "--s") == 0)
+	        {
+	        	print_mode.isSymlink = 1;
 	        }
 	    }
 	    myls_l(argv[1], print_mode);
@@ -57,7 +61,13 @@ int main(int argc, char *argv[])
             print_mode.showIgnore = 1;
             getcwd(buf, sizeof(buf));										//获得当前路径
             myls_l(buf, print_mode);
-        } else
+        }else if (strcmp(argv[1], "--s") == 0)
+        {
+        	print_mode.isSymlink = 1;
+            getcwd(buf, sizeof(buf));										//获得当前路径
+            myls_l(buf, print_mode);
+        } 
+        else
         {
             myls_l(argv[1], print_mode);
         }
@@ -69,14 +79,20 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void print_dir(struct dirent *det)
+
+void print_dir(struct dirent *det, struct mode print_mode)
 {
     struct stat statbuf = {0};											//记录文件的属性
     struct passwd *pwd = NULL;											//记录文件所属用户
     struct group *grp = NULL;											//记录文件所属用户组
     struct tm mtm = {0};
-
-    stat(det->d_name, &statbuf);
+    if (print_mode.isSymlink == 1)
+    {
+		lstat(det->d_name, &statbuf);
+    }
+    else{
+    	stat(det->d_name, &statbuf);
+    }
     //st_mode													//文件类型
     if(S_ISREG(statbuf.st_mode))
         printf("-");
@@ -154,11 +170,11 @@ void print_dir(struct dirent *det)
 void myls_l(char *pathname, struct mode print_mode) {
     printf("%s\n", pathname);
     printf("p：%d i:%d\n",print_mode.showPoint, print_mode.showIgnore);
+
     DIR *cwd = NULL;                                                    //路径文件指针
     struct dirent *det = NULL;                                            //记录读到的文件信息
-
     chdir(pathname);                                                    //!非常重要，否则无法获取文件属性
-    cwd = opendir(pathname);                                            //打开目录
+    cwd = opendir(pathname);											//打开目录
     while (NULL != (det = readdir(cwd)))                                    //遍历当前目录
     {
         int isprint = 1;
@@ -175,7 +191,17 @@ void myls_l(char *pathname, struct mode print_mode) {
                 isprint = 0;
         }
         if (isprint == 1)
-            print_dir(det);
+            print_dir(det, print_mode);
     }
     closedir(cwd);                                                        //关闭目录
 }
+
+/*
+if(det->d_type == 10)
+{
+	char path = filename;
+	path = det->d_name+filename;
+
+}
+
+*/
